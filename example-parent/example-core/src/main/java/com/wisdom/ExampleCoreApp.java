20997d4280e5012039d9829822c0a41483bc298d
yangshangwei <749969369@qq.com>
2023-07-28 17:56:05 +0800

    测试

--- example-parent/example-core/src/main/java/com/wisdom/ExampleCoreApp.java
+++ example-parent/example-core/src/main/java/com/wisdom/ExampleCoreApp.java
@@ -1,12 +1,11 @@
 /*
  * Copyright (c) 2023 dragonSaberCaptain.All rights reserved.
- * 版权创建时间:2023/7/28 下午5:39
  * 当前项目名:wisdom-parent
  * 当前模块名:example-core
  * 当前文件的权限定名:com.wisdom.ExampleCoreApp
  * 当前文件的名称:ExampleCoreApp.java
  * 当前文件的类名:ExampleCoreApp
- * 上一次文件修改的日期时间:2023/7/28 下午5:38
+ * 上一次文件修改的日期时间:2023/7/28 下午5:44
  *
  */
 
